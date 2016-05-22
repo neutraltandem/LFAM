2c8780685380e590a3d7d0770479fe51454ae430
Chris Rutledge <christopher.rutledge@gmail.com>
2016-05-22 11:37:42 -0400

    Second commit

--- LFAM/settings.py
+++ LFAM/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'LFAM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lfam',
+        'USER': 'lfamapp',
+        'PASSWORD': 'temp1234',
+        'HOST': 'localhost',
     }
 }
+
+
 
 
 # Internationalization
